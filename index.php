8179d02c916f2f89e59fc12a3dc6b8cd500cd98f
Đặng Quang Khánh Linh <117424587+danglinh152@users.noreply.github.com>
2024-05-20 22:09:46 +0700

    optimizecode

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ require_once "includes/connect.php";
 require_once "includes/database.php";
 require_once "includes/session.php";
 $CODE = true;
-
 $module = _MODULE;
 $action = _ACTION;
 if (!empty($_GET['module'])) {
@@ -14,13 +13,11 @@ if (!empty($_GET['module'])) {
         $module = trim($_GET['module']);
     }
 }
-
 if (!empty($_GET['action'])) {
     if (is_string($_GET['action'])) {
         $action = trim($_GET['action']);
     }
 }
-
 $path = 'modules/' . $module . '/' . $action . '.php';
 if (file_exists($path))
     require_once($path);
